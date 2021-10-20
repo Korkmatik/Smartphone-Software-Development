36b9ef23a3ef54e8c16cb89780510740f543b39f
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2021-10-20 12:20:37 +0200

    Add Ggt calculator

---
+++ Reduce/app/src/main/java/com/example/reduce/utils/Ggt.java
@@ -0,0 +1,24 @@
+package com.example.reduce.utils;
+
+public class Ggt {
+
+    public static int calculate(int zaehler, int nenner) {
+        int ggt = 1;
+
+        if (zaehler * nenner != 0) {
+            int rest;
+            ggt = Math.abs(zaehler);
+            int divisor = Math.abs(nenner);
+            do {
+                rest = ggt % divisor;
+                ggt = divisor;
+                divisor = rest;
+            } while (rest > 0);
+
+            zaehler /= ggt;
+            nenner /= ggt;
+        }
+
+        return ggt;
+    }
+}
