9a77430b2beb665642aef94d3cdae1e27b327b6f
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2021-10-27 14:39:42 +0200

    Finish exercise 6

---
+++ Lifecycle/app/src/main/java/com/example/lifecycle/LifeActivity.java
@@ -0,0 +1,72 @@
+package com.example.lifecycle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+import javax.security.auth.login.LoginException;
+
+public class LifeActivity extends AppCompatActivity {
+
+    public final static String TAG = "LifeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.life);
+
+        Log.i(TAG, "onCreate: " + (savedInstanceState == null ? "NEW" : "RESTORED"));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.i(TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy");
+    }
+}
