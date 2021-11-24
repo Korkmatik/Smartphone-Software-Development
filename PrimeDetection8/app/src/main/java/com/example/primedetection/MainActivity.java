82b37f854002388af01e5ff59323b67c1f09a9d1
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2021-11-24 12:39:49 +0100

    Finishe exercise 8 1-5

---
+++ PrimeDetection8/app/src/main/java/com/example/primedetection/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.primedetection;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etUserInput;
+    TextView lblResult;
+    Button btnCheckPrime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViews();
+
+        // Resetting result text
+        lblResult.setText("");
+
+        addOnClickListenerToBtn();
+    }
+
+    private void addOnClickListenerToBtn() {
+        btnCheckPrime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = etUserInput.getText().toString();
+                if (!userInput.matches("-?\\d+")) {
+                    PrintError(userInput, userInput + " " + getString(R.string.not_integer));
+                }
+
+                try {
+                    long inputNumber = Integer.parseInt(userInput);
+                    long divisor = getBiggestDivisor(Math.abs(inputNumber));
+
+                    if (divisor == inputNumber || divisor == 1) {
+                        lblResult.setText(userInput + " " + getString(R.string.is_prime));
+                    } else {
+                        lblResult.setText(
+                                userInput + " " + getString(R.string.not_prime) +
+                                        "\n(" + getString(R.string.divisor) + ": " + divisor + ")"
+                        );
+                    }
+                } catch (java.lang.NumberFormatException e){
+                    PrintError(userInput, userInput + " " + getString(R.string.not_integer));
+                }
+            }
+        });
+    }
+
+    private void PrintError(String userInput, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setPositiveButton(getString(R.string.ok), null);
+        builder.setMessage(message);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void findViews() {
+        etUserInput = findViewById(R.id.etPrimeNumber);
+        lblResult = findViewById(R.id.lblResult);
+        btnCheckPrime = findViewById(R.id.btnCheckPrime);
+    }
+
+    private long getBiggestDivisor(long number) {
+        long numberToCheck = number;
+        while (numberToCheck > 1) {
+            numberToCheck--;
+
+            if (number % numberToCheck == 0) {
+                return numberToCheck;
+            }
+        }
+
+        return number;
+    }
+}
