8995e1042a33aa86bb71bf16897d4667f5a4fe59
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2022-01-11 20:19:34 +0100

    Finish exercise 8.3

--- PrimeDetection2/app/src/main/java/com/example/primedetection2/MainActivity.java
+++ PrimeDetection2/app/src/main/java/com/example/primedetection2/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.primedetection2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnCheckPrime;
-    private EditText userInput;
+    private AutoCompleteTextView userInput;
     private TextView txtResult;
 
     @Override
@@ -23,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
         userInput = findViewById(R.id.etNumber);
         txtResult = findViewById(R.id.txtResult);
 
+        String[] primzahlen = {
+                "7", "11", "2"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, primzahlen);
+        userInput.setAdapter(adapter);
+
         txtResult.setText("");
 
         btnCheckPrime.setOnClickListener(new View.OnClickListener() {
@@ -32,14 +44,17 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     number = Integer.parseInt(userInput.getText().toString());
                 } catch (Exception e) {
+                    txtResult.setText("Number " + getString(R.string.not_integer));
                     return;
                 }
 
                 int divisor = isPrime(number);
                 if (divisor != 1 && divisor != number) {
                     txtResult.setText(number + " " + getString(R.string.not_prime) + "\n(" + getString(R.string.divisor) + " " + divisor + ")");
+                    txtResult.setTypeface(Typeface.DEFAULT, Typeface.NORMAL);
                 } else {
                     txtResult.setText(number + " " + getString(R.string.is_prime));
+                    txtResult.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
                 }
             }
         });
