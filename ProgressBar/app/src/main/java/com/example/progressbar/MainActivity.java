217ac474b9493b7c3b96cab488c3b8ad529e8303
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2022-01-17 20:46:55 +0100

    Add Menu

--- ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
+++ ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.progressbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,31 @@ public class MainActivity extends AppCompatActivity {
                 progressDemoTask.execute(progressBar);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        //menu.setGroupVisible(R.id.my_items_grp, false);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_add:
+                Log.i("MainActivity", "Add clicked");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void takePicture(MenuItem menuItem) {
+        Log.i("MainActivity", "Take picture clicked");
     }
 }
